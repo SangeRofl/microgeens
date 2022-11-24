2980071fc24ad13e15ea59de52150c49f5132f73
Alexandr <linkevich2002@mail.ru>
2022-11-24 18:08:39 +0300

    v 0.3

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask("microgreens")
-
-@app.route('/')
+prods = ["Яблоко", "Апельсин", "Груша"]
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html', products = prods)
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return 'hello moto-moto'
 
 if __name__ == "__main__":
     app.run(debug = True)
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 Для запуска откройте cmd в текущей папке
 введите "venv\Scripts\activate"
+
 введите "python .\app.py"
+
 откройте в браузере "localhost:5000"
